5278b3469bdf722c65e0fa708c6b864795aa26f6
Gellért Császár <csaszargellert@gmail.com>
2023-12-26 09:45:02 +0100

    create .env file with server url

--- src/components/utils/axios.js
+++ src/components/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AuthProvider from "./auth";
 import refreshToken from "./refreshToken";
 
-const baseURL = "http://localhost:8080/api";
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
 const axiosBase = axios.create({
   baseURL,
