033443fb679ed0b0764f88f1da7e0f5b30e5a5a0
Gellért Császár <csaszargellert@gmail.com>
2024-01-14 12:22:01 +0100

    add blank line to check if github actions executes npm ci --omit=dev command

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-require("dotenv").config({ path: "config.env" });
-const app = require("./app");
+require('dotenv').config({ path: 'config.env' });
+const app = require('./app');
 
 const dbConnection = process.env.DB_CONNECTION;
+
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const port = process.env.PORT || 3000;
 
 const DB = dbConnection
-  .replace("<username>", dbUsername)
-  .replace("<password>", dbPassword);
+  .replace('<username>', dbUsername)
+  .replace('<password>', dbPassword);
 
 let server;
 mongoose
@@ -26,25 +27,25 @@ mongoose
   });
 
 // Connection to DB was successful
-mongoose.connection.on("connected", function () {
-  console.log("Mongoose has connected to DB successfully");
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose has connected to DB successfully');
 });
 
 // Error after initial connection was established
-mongoose.connection.on("error", function (error) {
+mongoose.connection.on('error', function (error) {
   console.error(`Some error happened after initial connection: ${error}`);
 });
 
 // Mongoose disconnected
-mongoose.connection.on("disconnected", function (error) {
+mongoose.connection.on('disconnected', function (error) {
   console.error(`Mongoose has disconnected: ${error}`);
 });
 
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
   server.close(() => {
-    console.log("App has been closed gracefully.");
+    console.log('App has been closed gracefully.');
     mongoose.connection.close(() => {
-      console.log("Mongoose connection closed due to app termination");
+      console.log('Mongoose connection closed due to app termination');
     });
   });
 });
